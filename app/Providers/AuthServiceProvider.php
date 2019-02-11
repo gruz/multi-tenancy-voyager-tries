d1ca4faff374b3065dbcd9fd5884ba1da884c649
Gruz <arygroup@gmail.com>
2019-02-11 17:58:25 +0200

    Remove unneded staff

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,10 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
-use TCG\Voyager\Policies\BasePolicy;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use TCG\Voyager\Facades\Voyager as VoyagerFacade;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -17,8 +14,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
-        // 'TCG\Voyager\Models\Setting' => 'TCG\Voyager\Policies\SettingPolicy',
-        // 'TCG\Voyager\Models\MenuItem' => 'TCG\Voyager\Policies\MenuItemPolicy',
     ];
 
     /**
@@ -29,26 +24,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-        return;
-        try {
-            if (Schema::hasTable('data_types')) {
-                $dataType = VoyagerFacade::model('DataType');
-                $dataTypes = $dataType->select('policy_name', 'model_name')->get();
 
-                foreach ($dataTypes as $dataType) {
-                    $policyClass = BasePolicy::class;
-                    if (isset($dataType->policy_name) && $dataType->policy_name !== ''
-                        && class_exists($dataType->policy_name)) {
-                        $policyClass = $dataType->policy_name;
-                    }
-
-                    $this->policies[$dataType->model_name] = $policyClass;
-                }
-
-                $this->registerPolicies();
-            }
-        } catch (\PDOException $e) {
-            Log::error('No Database connection yet in VoyagerServiceProvider registerGates()');
-        }
+        //
     }
 }
