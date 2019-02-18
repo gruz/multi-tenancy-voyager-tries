8500103e9fac10377cc0814be3fc3c354fc40504
Gruz <arygroup@gmail.com>
2019-02-18 07:22:51 +0200

    Restores missing sections in templates

--- resources/views/vendor/voyager/hostnames/browse.blade.php
+++ resources/views/vendor/voyager/hostnames/browse.blade.php
@@ -1,5 +1,31 @@
 @extends('voyager::master')
 
+@section('page_title', __('voyager::generic.viewing').' '.$dataType->display_name_plural)
+
+@section('page_header')
+    <div class="container-fluid">
+        <h1 class="page-title">
+            <i class="{{ $dataType->icon }}"></i> {{ $dataType->display_name_plural }}
+        </h1>
+        @can('add', app($dataType->model_name))
+            <a href="{{ route('voyager.'.$dataType->slug.'.create') }}" class="btn btn-success btn-add-new">
+                <i class="voyager-plus"></i> <span>{{ __('voyager::generic.add_new') }}</span>
+            </a>
+        @endcan
+        @can('delete', app($dataType->model_name))
+            @include('voyager::partials.bulk-delete')
+        @endcan
+        @can('edit', app($dataType->model_name))
+            @if(isset($dataType->order_column) && isset($dataType->order_display_column))
+                <a href="{{ route('voyager.'.$dataType->slug.'.order') }}" class="btn btn-primary">
+                    <i class="voyager-list"></i> <span>{{ __('voyager::bread.order') }}</span>
+                </a>
+            @endif
+        @endcan
+        @include('voyager::multilingual.language-selector')
+    </div>
+@stop
+
 @section('content')
     <div class="page-content browse container-fluid">
         @include('voyager::alerts')
@@ -211,4 +237,53 @@
             </div><!-- /.modal-content -->
         </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
+@stop
+
+@section('css')
+@if(!$dataType->server_side && config('dashboard.data_tables.responsive'))
+    <link rel="stylesheet" href="{{ voyager_asset('lib/css/responsive.dataTables.min.css') }}">
+@endif
+@stop
+
+@section('javascript')
+    <!-- DataTables -->
+    @if(!$dataType->server_side && config('dashboard.data_tables.responsive'))
+        <script src="{{ voyager_asset('lib/js/dataTables.responsive.min.js') }}"></script>
+    @endif
+    <script>
+        $(document).ready(function () {
+            @if (!$dataType->server_side)
+                var table = $('#dataTable').DataTable({!! json_encode(
+                    array_merge([
+                        "order" => $orderColumn,
+                        "language" => __('voyager::datatable'),
+                        "columnDefs" => [['targets' => -1, 'searchable' =>  false, 'orderable' => false]],
+                    ],
+                    config('voyager.dashboard.data_tables', []))
+                , true) !!});
+            @else
+                $('#search-input select').select2({
+                    minimumResultsForSearch: Infinity
+                });
+            @endif
+
+            @if ($isModelTranslatable)
+                $('.side-body').multilingual();
+                //Reinitialise the multilingual features when they change tab
+                $('#dataTable').on('draw.dt', function(){
+                    $('.side-body').data('multilingual').init();
+                })
+            @endif
+            $('.select_all').on('click', function(e) {
+                $('input[name="row_id"]').prop('checked', $(this).prop('checked'));
+            });
+        });
+
+
+        var deleteFormAction;
+        $('td').on('click', '.delete', function (e) {
+            $('#delete_form')[0].action = '{{ route('voyager.'.$dataType->slug.'.destroy', ['id' => '__id']) }}'.replace('__id', $(this).data('id'));
+            $('#delete_modal').modal('show');
+        });
+    </script>
 @stop
--- resources/views/vendor/voyager/hostnames/read.blade.php
+++ resources/views/vendor/voyager/hostnames/read.blade.php
@@ -1,5 +1,31 @@
 @extends('voyager::master')
 
+@section('page_title', __('voyager::generic.view').' '.$dataType->display_name_singular)
+
+@section('page_header')
+    <h1 class="page-title">
+        <i class="{{ $dataType->icon }}"></i> {{ __('voyager::generic.viewing') }} {{ ucfirst($dataType->display_name_singular) }} &nbsp;
+
+        @can('edit', $dataTypeContent)
+            <a href="{{ route('voyager.'.$dataType->slug.'.edit', $dataTypeContent->getKey()) }}" class="btn btn-info">
+                <span class="glyphicon glyphicon-pencil"></span>&nbsp;
+                {{ __('voyager::generic.edit') }}
+            </a>
+        @endcan
+        @can('delete', $dataTypeContent)
+            <a href="javascript:;" title="{{ __('voyager::generic.delete') }}" class="btn btn-danger" data-id="{{ $dataTypeContent->getKey() }}" id="delete-{{ $dataTypeContent->getKey() }}">
+                <i class="voyager-trash"></i> <span class="hidden-xs hidden-sm">{{ __('voyager::generic.delete') }}</span>
+            </a>
+        @endcan
+
+        <a href="{{ route('voyager.'.$dataType->slug.'.index') }}" class="btn btn-warning">
+            <span class="glyphicon glyphicon-list"></span>&nbsp;
+            {{ __('voyager::generic.return_to_list') }}
+        </a>
+    </h1>
+    @include('voyager::multilingual.language-selector')
+@stop
+
 @section('content')
     <div class="page-content read container-fluid">
         <div class="row">
@@ -117,4 +143,33 @@
             </div><!-- /.modal-content -->
         </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
+@stop
+
+@section('javascript')
+    @if ($isModelTranslatable)
+        <script>
+            $(document).ready(function () {
+                $('.side-body').multilingual();
+            });
+        </script>
+        <script src="{{ voyager_asset('js/multilingual.js') }}"></script>
+    @endif
+    <script>
+        var deleteFormAction;
+        $('.delete').on('click', function (e) {
+            var form = $('#delete_form')[0];
+
+            if (!deleteFormAction) {
+                // Save form action initial value
+                deleteFormAction = form.action;
+            }
+
+            form.action = deleteFormAction.match(/\/[0-9]+$/)
+                ? deleteFormAction.replace(/([0-9]+$)/, $(this).data('id'))
+                : deleteFormAction + '/' + $(this).data('id');
+
+            $('#delete_modal').modal('show');
+        });
+
+    </script>
 @stop
